c67a0c6fc04ce701bf9eb0be99ce6c1c2d6dee78
jeff <rjojr@protonmail.com>
2024-11-18 09:24:17 -0500

    working on tetromino rotation

---
+++ GameRenderer.hpp
@@ -0,0 +1,29 @@
+// class for handling the window the game will be rendered to
+#ifndef GAMERENDERER_H
+#define GAMERENDERER_H
+
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+class GameRenderer
+{
+public:
+    GameRenderer(std::string name, int window_width, int window_height);
+    SDL_Texture *loadTexture(std::string tex_path);
+    void destroy();
+    void clear();
+    void changeColor(int* rgb);
+    void renderLine(int* coords);
+    void renderRects(SDL_Rect *rects, int size=4);
+    void renderImage(SDL_Texture *tex, SDL_Rect *src, SDL_Rect *dst);
+    void renderText(TTF_Font *font, std::string text, SDL_Rect *src, SDL_Rect *dst);
+    void display();
+
+private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+
+#endif
---
+++ gamerenderer.cpp
@@ -0,0 +1,83 @@
+// implementation of GameRenderer class
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
+#include <cstring>
+#include <stdio.h>
+
+#include "GameRenderer.hpp"
+
+GameRenderer::GameRenderer(std::string name, int window_width, int window_height)
+    : window{nullptr}, renderer{nullptr}
+{
+    window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              window_width, window_height, SDL_WINDOW_SHOWN);
+
+    if (window == nullptr)
+    {
+        printf("Window failed to initialize! SDL Error: %s\n", SDL_GetError());
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
+
+SDL_Texture *GameRenderer::loadTexture(std::string tex_path)
+{
+
+    SDL_Texture *gTexture = NULL;
+
+    gTexture = IMG_LoadTexture(renderer, tex_path.c_str());
+
+    if (gTexture == NULL)
+    {
+        printf("Unable to create texture from %s! SDL Error: %s\n", tex_path.c_str(), SDL_GetError());
+    }
+
+    return gTexture;
+}
+
+void GameRenderer::destroy()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
+
+void GameRenderer::clear()
+{
+    SDL_RenderClear(renderer);
+}
+
+void GameRenderer::renderLine(int coords[4]) {
+    SDL_RenderDrawLine(renderer, coords[0], coords[1], coords[2], coords[3]);
+}
+
+void GameRenderer::renderRects(SDL_Rect *rects, int size)
+{
+    SDL_RenderFillRects(renderer, rects, size);
+}
+
+void GameRenderer::changeColor(int rgb[4])
+{
+    SDL_SetRenderDrawColor(renderer, rgb[0], rgb[1], rgb[2], rgb[3]);
+}
+void GameRenderer::renderImage(SDL_Texture *tex, SDL_Rect *src, SDL_Rect *dst)
+{
+    SDL_RenderCopy(renderer, tex, src, dst);
+}
+
+void GameRenderer::renderText(TTF_Font *font, std::string text, SDL_Rect *src, SDL_Rect *dst)
+{
+    // create an sdl surface
+    SDL_Surface *textSurface = TTF_RenderText_Solid(font, text.c_str(), {0, 255, 0});
+    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    SDL_RenderCopy(renderer, textTexture, src, dst);
+    SDL_DestroyTexture(textTexture);
+    SDL_FreeSurface(textSurface);
+}
+
+void GameRenderer::display()
+{
+    SDL_RenderPresent(renderer);
+}
